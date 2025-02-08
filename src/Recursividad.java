a7d7d31ca6c9631596aceb633ced2d0e1d62b397
JuanDavidBedoya <142841824+JuanDavidBedoya@users.noreply.github.com>
2025-02-08 15:38:02 -0500

    Update Recursividad.java

--- src/Recursividad.java
+++ src/Recursividad.java
@@ -2,24 +2,24 @@ public class Recursividad {
 
     // Método main para ejecutar el código 
     public static void main(String[] args) {
-        int[] numeros = {1, 2, 3, 4};
-        int resultado = suma(numeros, 0, numeros.length-1);
+        int[] arreglo = {1, 2, 3, 4, 5, 6};
+        int resultado = suma(arreglo, 0, arreglo.length-1);
         System.out.println(resultado);
     }
 
     // Método recursivo que aplica la técinca divide y venceras
-    private static int suma(int[] numeros, int inicio, int fin) {
+    private static int suma(int[] arreglo, int inicio, int fin) {
         // Caso base, cuando solo hay un elemento en el arreglo
         if (inicio == fin) {
-            return numeros[inicio];
+            return arreglo[inicio];
         }
 
         // Encuentra el punto medio
         int medio = (inicio + fin) / 2;
 
         // Suma la parte izquierda y la parte derecha recursivamente
-        int sumaIzquierda = suma(numeros, inicio, medio);
-        int sumaDerecha = suma(numeros, medio + 1, fin);
+        int sumaIzquierda = suma(arreglo, inicio, medio);
+        int sumaDerecha = suma(arreglo, medio + 1, fin);
 
         // Suma los resultados de cada mitad
         return sumaIzquierda + sumaDerecha;
