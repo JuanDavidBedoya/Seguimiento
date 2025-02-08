e57fc7469320e587e64a224084c135453a2f9f91
JuanDavidBedoya <142841824+JuanDavidBedoya@users.noreply.github.com>
2025-02-08 15:39:17 -0500

    Seguimiento recursividad pila

--- src/Recursividad.java
+++ src/Recursividad.java
@@ -3,12 +3,12 @@ public class Recursividad {
     // Método main para ejecutar el código 
     public static void main(String[] args) {
         int[] arreglo = {1, 2, 3, 4, 5, 6};
-        int resultado = suma(arreglo, 0, arreglo.length-1);
+        int resultado = sumaRecursiva(arreglo, 0, arreglo.length-1);
         System.out.println(resultado);
     }
 
     // Método recursivo que aplica la técinca divide y venceras
-    private static int suma(int[] arreglo, int inicio, int fin) {
+    private static int sumaRecursiva(int[] arreglo, int inicio, int fin) {
         // Caso base, cuando solo hay un elemento en el arreglo
         if (inicio == fin) {
             return arreglo[inicio];
@@ -18,8 +18,8 @@ public class Recursividad {
         int medio = (inicio + fin) / 2;
 
         // Suma la parte izquierda y la parte derecha recursivamente
-        int sumaIzquierda = suma(arreglo, inicio, medio);
-        int sumaDerecha = suma(arreglo, medio + 1, fin);
+        int sumaIzquierda = sumaRecursiva(arreglo, inicio, medio);
+        int sumaDerecha = sumaRecursiva(arreglo, medio + 1, fin);
 
         // Suma los resultados de cada mitad
         return sumaIzquierda + sumaDerecha;
